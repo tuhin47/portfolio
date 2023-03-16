f62fccbb4b95f5e8dc196216bba3019fb842fa9f
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-03-16 22:47:25 +0600

    fix routerlink

--- src/app/shared/utils/fade-in-out.ts
+++ src/app/shared/utils/fade-in-out.ts
@@ -4,9 +4,6 @@ export var FadeInOut = trigger('fadeInOut', [
   state('in', style({opacity: 1, transform: 'translateY(0)'})),
   transition('void => *', [
     style({opacity: 0, transform: 'translateY(75%)'}),
-    animate(500)
-  ]),
-  transition('* => void', [
-    animate(500, style({opacity: 0, transform: 'translateY(75%)'}))
+    animate(1000)
   ])
 ])
