4776894dcceafd41367573774a457a610b6a647c
Md. Towhidul Islam <towhidul@1it.com.au>
2023-10-08 09:54:54 +0600

    add dynamic experiences with header

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -8,28 +8,21 @@ import {SkillGroup} from "./skill-group/skill-group";
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
-  experiences?: Experience[];
-  educations?: Experience[];
-  publications?: Experience[];
-  extras?: Experience[];
-  isCompleted = true;
 
-
+  listOfExperiences?: any[];
   skillGroups!: SkillGroup[];
-  ngOnInit(): void {
-    // fetch("https://gist.githubusercontent.com/tuhin47/ee4d6449b4f7d0a71b39b2c5a08f6571/raw/experiences.json",
+  isCompleted = false;
 
-    fetch("http://localhost:9090/expeiences",
+  ngOnInit(): void {
+    fetch("https://gist.githubusercontent.com/tuhin47/ee4d6449b4f7d0a71b39b2c5a08f6571/raw/experiences.json",
+    // fetch("http://localhost:9090/expeiences",
       {
         method: 'GET',
         redirect: 'follow'
       })
       .then(response => response.json())
       .then(result => {
-        this.experiences = result.experiences;
-        this.educations = result.educations;
-        this.publications = result.publications;
-        this.extras = result.extras;
+        this.listOfExperiences = result.listOfExperiences;
         this.skillGroups = result.skillGroups;
         this.isCompleted = true;
         console.log(result)
